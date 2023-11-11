8e2a307c91ed641fdabf430688825a44de061a0a
JamesAndrewKong <jamesandrewkong@gmail.com>
2023-11-11 16:32:12 +0100

    Geluid Tests

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include "Objects/Scene.hpp"
 #include "BrackEngine.hpp"
 //#include "../Brack-Engine/src/FPSSingleton.hpp"
+#include "Components/AIComponent.hpp"
+#include "Components/AnimationComponent.hpp"
+#include "Components/AudioComponent.hpp"
 
 int main() {
     Config config = new Config();
@@ -13,11 +16,11 @@ int main() {
 
     BrackEngine brackEngine = BrackEngine(std::move(config));
     auto camera = Camera();
+
     camera.SetBackgroundColor(Color(0, 255, 0, 255));
     auto scene = Scene(std::move(camera));
 
     GameObject object = GameObject();
-
     SpriteComponent *sprite = new SpriteComponent();
 
     object.AddComponent(sprite);
@@ -30,6 +33,32 @@ int main() {
 
     SceneManager::GetInstance().SetActiveScene(scene);
 
+    AIComponent component = AIComponent();
+    AnimationComponent component2 = AnimationComponent();
+
+    AudioComponent component5 = AudioComponent();
+    component5.audioPath = "../Assets/Sounds/sound.mp3";
+    component5.isPlaying = true;
+    component5.volume = 1;
+    component5.shouldBePlaying = true;
+    component5.isLooping = false;
+
+    object.AddComponent(&component2);
+    object.AddComponent(&component);
+    object.AddComponent(&component5);
+    scene.AddGameObject(object);
+
+    SceneManager::GetInstance().SetActiveScene(scene);
+
+    object.RemoveComponent<AnimationComponent>();
+    camera.SetCameraComponent(new CameraComponent());
+    camera.SetBackgroundColor(Color(255, 0, 0, 255));
+    scene = Scene(std::move(camera));
+    scene.AddGameObject(object);
+
+    SceneManager::GetInstance().SetActiveScene(scene);
+
     brackEngine.Run();
+
     return 0;
 }
