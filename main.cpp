1724b4c4230a77276706c5da7fb9f0ddd41785cf
JamesAndrewKong <jamesandrewkong@gmail.com>
2023-11-14 18:44:38 +0100

    Updated to account for new development changes

--- main.cpp
+++ main.cpp
@@ -10,57 +10,30 @@
 #include "Components/AudioComponent.hpp"
 
 int main() {
+    std::unique_ptr<GameObject> object = std::make_unique<GameObject>();
+    int channel = 1;
+    auto audioPath = "../Assets/Sounds/song.mp3";
+    bool isSoundTrack = true;
+    std::unique_ptr<AudioComponent> component5 = std::make_unique<AudioComponent>(audioPath, channel, isSoundTrack);
+    component5->volume = 1;
+    component5->shouldBePlaying = true;
+    component5->isLooping = false;
+
+    object->AddComponent(std::move(component5));
+
     Config config = new Config();
     config.windowTitle = "Brackocalypse";
     config.windowSize = Vector2(800, 600);
+    config.showFPS = true;
 
     BrackEngine brackEngine = BrackEngine(std::move(config));
     auto camera = Camera();
-
     camera.SetBackgroundColor(Color(0, 255, 0, 255));
     auto scene = Scene(std::move(camera));
 
-    GameObject object = GameObject();
-    SpriteComponent *sprite = new SpriteComponent();
-
-    object.AddComponent(sprite);
-
-
-    Text text = Text("Poepjes", 40);
-
-    scene.AddGameObject(object);
-    scene.AddGameObject(text);
-
-    SceneManager::GetInstance().SetActiveScene(scene);
-
-    AIComponent component = AIComponent();
-    AnimationComponent component2 = AnimationComponent();
-
-    AudioComponent component5 = AudioComponent();
-    component5.audioPath = "../Assets/Sounds/sound.mp3";
-    component5.isPlaying = false;
-    component5.volume = 1;
-    component5.shouldBePlaying = true;
-    component5.isLooping = false;
-    component5.channel = 1;
-    component5.isSoundTrack = true;
-
-    object.AddComponent(&component2);
-    object.AddComponent(&component);
-    object.AddComponent(&component5);
-    scene.AddGameObject(object);
-
-    SceneManager::GetInstance().SetActiveScene(scene);
-
-    object.RemoveComponent<AnimationComponent>();
-    camera.SetCameraComponent(new CameraComponent());
-    camera.SetBackgroundColor(Color(255, 0, 0, 255));
-    scene = Scene(std::move(camera));
-    scene.AddGameObject(object);
-
+    scene.AddGameObject(std::move(object));
     SceneManager::GetInstance().SetActiveScene(scene);
 
     brackEngine.Run();
-
     return 0;
 }
