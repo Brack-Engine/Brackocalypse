1cc7e53f34f43ccceb0691ad4a7d935e770972fe
JamesAndrewKong <jamesandrewkong@gmail.com>
2023-11-17 00:02:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,12 +13,33 @@
 
 int main() {
     std::unique_ptr<GameObject> object = std::make_unique<GameObject>();
-    auto audioPath = "../Assets/Sounds/song.mp3";
-    std::unique_ptr<AudioComponent> component5 = std::make_unique<SoundEffectComponent>(audioPath);
+    auto audioPath = "../Assets/Sounds/sound.mp3";
+    std::unique_ptr<SoundEffectComponent> component5 = std::make_unique<SoundEffectComponent>(audioPath);
     component5->startPlaying = true;
     component5->volume = 0.1;
 
+    std::unique_ptr<SoundEffectComponent> component6 = std::make_unique<SoundEffectComponent>(audioPath);
+    component6->startPlaying = true;
+    component6->volume = 0.1;
+
+    std::unique_ptr<SoundEffectComponent> component7 = std::make_unique<SoundEffectComponent>(audioPath);
+    component7->startPlaying = true;
+    component7->volume = 0.1;
+
+    std::unique_ptr<SoundEffectComponent> component9 = std::make_unique<SoundEffectComponent>(audioPath);
+    component9->startPlaying = true;
+    component9->volume = 0.1;
+
+    auto audioPath2 = "../Assets/Sounds/song.mp3";
+    std::unique_ptr<SoundTrackComponent> component8 = std::make_unique<SoundTrackComponent>(audioPath2);
+    component8->startPlaying = true;
+    component8->volume = 0.1;
+
     object->AddComponent(std::move(component5));
+    object->AddComponent(std::move(component6));
+    object->AddComponent(std::move(component7));
+    object->AddComponent(std::move(component8));
+    object->AddComponent(std::move(component9));
 
     Config config = new Config();
     config.windowTitle = "Brackocalypse";
