cb5772f66e9adeeb9c1ea572d12cde353868aedb
Olaf van den Berg <oa.vandenberg@student.avans.nl>
2023-11-22 12:59:14 +0100

    Change collion handling

--- main.cpp
+++ main.cpp
@@ -2,53 +2,75 @@
 #include <Components/AIComponent.hpp>
 #include <Components/AnimationComponent.hpp>
 #include <Objects/Text.hpp>
-#include <Components/AIComponent.hpp>
-#include <Components/AnimationComponent.hpp>
+#include <Components/CircleCollisionComponent.hpp>
+#include <Components/TextComponent.hpp>
+#include <Components/BoxCollisionComponent.hpp>
 #include "Objects/Scene.hpp"
 #include "BrackEngine.hpp"
 //#include "../Brack-Engine/src/FPSSingleton.hpp"
-#include "../Brack-Engine/src/ConfigSingleton.hpp"
-#include "Components/AudioComponent.hpp"
 
 int main() {
     Config config = new Config();
     config.windowTitle = "Brackocalypse";
     config.windowSize = Vector2(800, 600);
-    config.fpsLimit = 60;
+    config.showFPS = false;
 
     BrackEngine brackEngine = BrackEngine(std::move(config));
     auto camera = Camera();
     camera.SetBackgroundColor(Color(0, 255, 0, 255));
     auto scene = Scene(std::move(camera));
 
-    auto object = std::make_unique<GameObject>();
-    auto audio = AudioComponent();
-    object->AddComponent(audio);
-    
-    auto text = std::make_unique<Text>("Poepjes");
+    std::unique_ptr<Text> gameObject1 = std::make_unique<Text>("test");
+    std::unique_ptr<Text> gameObject2 = std::make_unique<Text>("test2");
 
-    scene.AddGameObject(std::move(object));
-    scene.AddGameObject(std::move(text));
 
-    for (int i = 0; i < 10; ++i) {
-        auto object = std::make_unique<GameObject>();
-        auto sprite = std::make_unique<SpriteComponent>();
-        auto transform = std::make_unique<TransformComponent>();
-        sprite->spritePath =
-                ConfigSingleton::GetInstance().GetBaseAssetPath() + "Sprites/roguelikeSheet_transparent_1.bmp";
-        sprite->spriteSize = std::make_unique<Vector2>(16, 16);
-        sprite->tileOffset = std::make_unique<Vector2>(6, 0);
-        sprite->margin = 1;
+    std::unique_ptr<VelocityComponent> veloc1 = std::make_unique<VelocityComponent>();
+    std::unique_ptr<VelocityComponent> veloc2 = std::make_unique<VelocityComponent>();
 
-        transform->position = std::make_unique<Vector2>(i * 16, 10);
-        transform->scale = std::make_unique<Vector2>(1, 1);
 
-        object->AddComponent(std::move(sprite));
-        object->AddComponent(std::move(transform));
-        scene.AddGameObject(std::move(object));
-    }
+    std::unique_ptr<CircleCollisionComponent> comp1 = std::make_unique<CircleCollisionComponent>(5);
+    std::unique_ptr<CircleCollisionComponent> comp2 = std::make_unique<CircleCollisionComponent>(5);
+    comp1->radius = std::make_unique<Vector2>(10.0f, 10.0f);
+    comp2->radius = std::make_unique<Vector2>(10.0f, 10.0f);
+
+//    std::unique_ptr<BoxCollisionComponent> comp1 = std::make_unique<BoxCollisionComponent>(Vector2(5, 5));
+    //std::unique_ptr<BoxCollisionComponent> comp2 = std::make_unique<BoxCollisionComponent>(Vector2(5, 5));
+//    comp1->size = std::make_unique<Vector2>(5.0f, 5.0f);
+    //comp2->size = std::make_unique<Vector2>(5.0f, 5.0f);
+
+    comp1->collisionType = CollisionType::DYNAMIC;
+    comp2->collisionType = CollisionType::DYNAMIC;
+
+//    Left -- Right
+/*    gameObject1->TryGetComponent<TransformComponent>().position = std::make_unique<Vector2>(50.0f, 50.0f);
+    gameObject2->TryGetComponent<TransformComponent>().position = std::make_unique<Vector2>(5.0f, 50.0f);
+
+    veloc1->velocity = Vector2(-10.0f, 0.0f);
+    veloc2->velocity = Vector2(10.0f, 0.0f);*/
+
+    //    Up -- Down
+    gameObject1->TryGetComponent<TransformComponent>().position = std::make_unique<Vector2>(50.0f, 5.0f);
+    gameObject2->TryGetComponent<TransformComponent>().position = std::make_unique<Vector2>(50.0f, 50.0f);
+
+    veloc1->velocity = Vector2(0.0f, 10.0f);
+    veloc2->velocity = Vector2(0.0f, -10.0f);
+
+    gameObject1->TryGetComponent<TextComponent>().sortingLayer = 1;
+    gameObject2->TryGetComponent<TextComponent>().sortingLayer = 1;
+
+
+    gameObject1->AddComponent(std::move(comp1));
+    gameObject1->AddComponent(std::move(veloc1));
+
+    gameObject2->AddComponent(std::move(comp2));
+    gameObject2->AddComponent(std::move(veloc2));
+
+
+    scene.AddGameObject(std::move(gameObject1));
+    scene.AddGameObject(std::move(gameObject2));
 
     SceneManager::GetInstance().SetActiveScene(scene);
+
 
     brackEngine.Run();
     return 0;
